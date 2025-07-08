b539adf420d59612970050dbfd84d58d2df1cfc1
SherifAshrf98 <sherifashrf6060@gmail.com>
2025-07-08 21:44:32 +0300

    Add Identity Models

---
+++ Examination.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace Examination.Domain.Entities.Identity
+{
+	public class AppUser : IdentityUser
+	{
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+	}
+}
