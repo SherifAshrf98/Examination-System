aaf11b20627690907dcf4b38dae8ca926ed5bb54
SherifAshrf98 <sherifashrf6060@gmail.com>
2025-07-11 12:19:17 +0300

    Seeding Initial Roles with Admin and student users

--- Examination.Api/Program.cs
+++ Examination.Api/Program.cs
@@ -1,6 +1,6 @@
-
 using Examination.Domain.Entities.Identity;
 using Examination.Infrastructure.Data;
+using Examination.Infrastructure.Data.Seeding;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +8,7 @@ namespace Examination.Api
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
@@ -29,12 +29,30 @@ namespace Examination.Api
 
 			var app = builder.Build();
 
+
+			var scope = app.Services.CreateScope();
+
+			var services = scope.ServiceProvider;
+
+			var logger = services.GetRequiredService<ILogger<Program>>();
+			try
+			{
+				var userManager = services.GetRequiredService<UserManager<AppUser>>();
+
+				var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+				await IdentitySeed.SeedIdentityAsync(userManager, roleManager,logger);
+			}
+			catch (Exception ex)
+			{
+				logger.LogError(ex, "An error occurred during seeding Identity Data");
+			}
+
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
 				app.UseSwaggerUI();
-
 			}
 
 			app.UseHttpsRedirection();
---
+++ Examination.Infrastructure/Data/Seeding/IdentitySeed.cs
@@ -0,0 +1,77 @@
+﻿using Examination.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace Examination.Infrastructure.Data.Seeding
+{
+	public static class IdentitySeed
+	{
+		public static async Task SeedIdentityAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, ILogger logger)
+		{
+			if (!await roleManager.RoleExistsAsync("Admin"))
+			{
+				var role = new IdentityRole("Admin");
+
+				await roleManager.CreateAsync(role);
+			}
+
+			if (!await roleManager.RoleExistsAsync("Student"))
+			{
+				var role = new IdentityRole("Student");
+
+				await roleManager.CreateAsync(role);
+			}
+
+			if (await userManager.FindByEmailAsync("sherifashrf6060@gmail.com") == null)
+			{
+				var AdminUser = new AppUser
+				{
+					UserName = "Admin_sherif",
+					Email = "sherifashrf6060@gmail.com"
+				};
+
+				var CreationResult = await userManager.CreateAsync(AdminUser, "CmPunk@98");
+
+				if (!CreationResult.Succeeded)
+				{
+					foreach (var Error in CreationResult.Errors)
+					{
+						logger.LogError($"Error creating admin user: {Error.Description}");
+					}
+
+					return;
+				}
+
+				if (!await userManager.IsInRoleAsync(AdminUser, "Admin"))
+				{
+					await userManager.AddToRoleAsync(AdminUser, "Admin");
+				}
+			}
+
+			if (await userManager.FindByEmailAsync("sherifashrf5050@gmail.com") == null)
+			{
+				var StudentUser = new AppUser
+				{
+					UserName = "Sherif2026",
+					Email = "sherifashrf5050@gmail.com"
+				};
+
+				var CreationResult = await userManager.CreateAsync(StudentUser, "Sherif0101516@");
+
+				if (!CreationResult.Succeeded)
+				{
+					foreach (var Error in CreationResult.Errors)
+					{
+						logger.LogError($"Error creating admin user: {Error.Description}");
+					}
+					return;
+				}
+
+				if (!await userManager.IsInRoleAsync(StudentUser, "Student"))
+				{
+					await userManager.AddToRoleAsync(StudentUser, "Student");
+				}
+			}
+		}
+	}
+}
